8577285971ed665e43c4b238cffb1eeea6461a44
Ahmed Karim <muhammadahmedkarimnagda@gmail.com>
2023-08-24 19:28:45 +0500

    add main heading

--- src/App.js
+++ src/App.js
@@ -2,19 +2,28 @@ import logo from "./logo.svg";
 import "./App.css";
 import MainNavbar from "./compo/navbar";
 import { DetailDiv } from "./compo/contant";
-import  Developer_gif  from "./assits/developer-gif.gif";
+import Developer_gif from "./assits/developer-gif.gif";
 
 function App() {
   return (
     <>
       <MainNavbar />
       <div className="main-div container">
+          <div>
+            <h3 className="div-heading">About Me</h3>
+          </div>
         <div className="d-flex first-div">
           <div className="detail-div">
             <DetailDiv />
           </div>
           <img className="developer-img" src={Developer_gif} alt="" />
         </div>
+        <div>
+            <h3 className="div-heading">Skill</h3>
+          </div>
+          <div>
+            
+          </div>
       </div>
     </>
   );
--- src/compo/contant.js
+++ src/compo/contant.js
@@ -2,11 +2,12 @@ import "./contant.css";
 
 const DetailDiv = () => (
   <>
-    <h2>Hi, I'm <span className="myName">Muhammad Ahmed</span></h2>
+    <h2>
+      Hi, I'm <span className="myName">Muhammad Ahmed</span>
+    </h2>
     <p className="detail-pra">
-      I was born on February 12th, 2001. I am a
-      skilled web developer with a passion for creating efficient and visually
-      appealing websites.
+      I was born on February 12th, 2001. I am a skilled web developer with a
+      passion for creating efficient and visually appealing websites.
     </p>
   </>
 );
